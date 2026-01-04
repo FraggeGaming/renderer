30fd256ae8d305fb1eb133a2507e12ecdbc37e60
FraggeGaming <Fardisnazem@gmail.com>
2026-01-04 16:17:48 +0100

    Tested the chunking, however need to refactor

--- Engine/ChunkManager.h
+++ Engine/ChunkManager.h
@@ -8,10 +8,10 @@
 
 using Entity = int;
 struct ChunkPos{
-    glm::vec3 position;
+    int x, y, z;
 
     bool operator==(const ChunkPos& other) const {
-        return position == other.position;
+        return x == other.x && y == other.y && z == other.z;
     }
 };
 
@@ -19,10 +19,9 @@ namespace std {
     template <>
     struct hash<ChunkPos> {
         size_t operator()(const ChunkPos& p) const {
-            // A simple bit-shifting combine hash
-            size_t h1 = hash<int>{}(p.position.x);
-            size_t h2 = hash<int>{}(p.position.y);
-            size_t h3 = hash<int>{}(p.position.z);
+            size_t h1 = hash<int>{}(p.x);
+            size_t h2 = hash<int>{}(p.y);
+            size_t h3 = hash<int>{}(p.z);
             return h1 ^ (h2 << 1) ^ (h3 << 2); 
         }
     };
@@ -31,34 +30,93 @@ namespace std {
 struct ChunkManager{
     std::unordered_map<ChunkPos, std::vector<Entity>> chunks;
     float chunkSize = 10.0f;
+    int loadRadius = 2;
 
-    ChunkPos currentLoaded;
+    std::vector<ChunkPos> loadedChunks;
 
+
+    //Load chunk based on transform position 
     void Load(TransformComponent& t){
-        ChunkPos pos = {glm::floor(t.position / chunkSize) * chunkSize};
-        currentLoaded = pos;
+        loadedChunks.clear();
+
+        for(int x = -loadRadius; x <= loadRadius; x++){
+            for(int y = -loadRadius; y <= loadRadius; y++){
+                for(int z = -loadRadius; z <= loadRadius; z++){
+                    ChunkPos pos = {
+                        (int)glm::floor(t.position.x / chunkSize) + x,
+                        (int)glm::floor(t.position.y / chunkSize) + y,
+                        (int)glm::floor(t.position.z / chunkSize) + z
+                    };
+                    if(std::find(loadedChunks.begin(), loadedChunks.end(), pos) == loadedChunks.end()){
+                        loadedChunks.push_back(pos);
+                    }
+                }
+            }
+        }
+        
     }
 
-    void Unload(TransformComponent& t){
-        ChunkPos pos = {glm::floor(t.position / chunkSize) * chunkSize};
-        chunks.erase(pos);
-
+    std::vector<Entity> GetAll(){
+        std::vector<Entity> entities;
+        for (const auto& [chunkPos, chunkEntities] : chunks) {
+            entities.insert(entities.end(), chunkEntities.begin(), chunkEntities.end());
+        }
+        return entities;
     }
 
-    bool InCurrectChunk(TransformComponent& t){
-        ChunkPos pos = {glm::floor(t.position / chunkSize) * chunkSize};
-        return pos.position == currentLoaded.position;
+    std::vector<Entity> GetLoadedEntities(){
+        std::vector<Entity> entities;
+        for (const auto& chunkPos : loadedChunks) {
+            auto& chunkEntities = chunks[chunkPos];
+            entities.insert(entities.end(), chunkEntities.begin(), chunkEntities.end());
+        }
+        return entities;
+    }
+
+
+    //TODO
+    bool Update(TransformComponent& t){
+        //Update chunk if entity has moved to a new chunk
+        ChunkPos currentPos = {
+            (int)glm::floor(t.position.x / chunkSize),
+            (int)glm::floor(t.position.y / chunkSize),
+            (int)glm::floor(t.position.z / chunkSize)
+        };
+
+        
     }
 
     void AddToChunk(TransformComponent& t, Entity e){
-        ChunkPos pos = {glm::floor(t.position / chunkSize) * chunkSize};
+        ChunkPos pos = {
+            (int)glm::floor(t.position.x / chunkSize),
+            (int)glm::floor(t.position.y / chunkSize),
+            (int)glm::floor(t.position.z / chunkSize)
+        };
         chunks[pos].push_back(e);
     }
 
     void RemoveFromChunk(TransformComponent& t, Entity e){
-        ChunkPos pos = {glm::floor(t.position / chunkSize) * chunkSize};
+        ChunkPos pos = {
+            (int)glm::floor(t.position.x / chunkSize),
+            (int)glm::floor(t.position.y / chunkSize),
+            (int)glm::floor(t.position.z / chunkSize)
+        };
         auto& vec = chunks[pos];
         vec.erase(std::remove(vec.begin(), vec.end(), e), vec.end());
+    }
+
+    void Unload(TransformComponent& t){
+        ChunkPos pos = {
+            (int)glm::floor(t.position.x / chunkSize),
+            (int)glm::floor(t.position.y / chunkSize),
+            (int)glm::floor(t.position.z / chunkSize)
+        };
+        for (auto it = loadedChunks.begin(); it != loadedChunks.end(); ++it) {
+            if (*it == pos) {
+                loadedChunks.erase(it);
+                break;
+            }
+        }
     }
 };
 
--- Engine/ECS/Systems/BatchedRenderer.cpp
+++ Engine/ECS/Systems/BatchedRenderer.cpp
@@ -47,6 +47,8 @@ void BatchedRenderer::LoadMesh(Entity id, BufferedMesh &m, glm::mat4 t)
 
     engine->ecs->AddComponent<GPUMemoryHandle>(id, handle);
     m.isLoaded = true;
+
+    chunkMgr.AddToChunk(engine->ecs->GetComponent<TransformComponent>(id), id);
 }
 
 void BatchedRenderer::Unload(Entity id)
@@ -57,6 +59,8 @@ void BatchedRenderer::Unload(Entity id)
     GPUMemoryHandle& handle = ecs->GetComponent<GPUMemoryHandle>(id);
     batch->Unload(handle);
     ecs->RemoveComponent<GPUMemoryHandle>(id);
+
+    chunkMgr.RemoveFromChunk(ecs->GetComponent<TransformComponent>(id), id);
     
 }
 
@@ -69,11 +73,7 @@ void BatchedRenderer::Start()
 {
     ecs->view<BufferedMesh, TransformComponent>().each([&](int entityId, BufferedMesh& m, TransformComponent& t) {
         if(m.isLoaded == false){
-            Mesh mesh = engine->assetManager.Get(m.meshID);
-            GPUMemoryHandle handle = batch->Load(m, mesh, t.GetCombined());
-
-            engine->ecs->AddComponent<GPUMemoryHandle>(entityId, handle);
-            m.isLoaded = true;
+            LoadMesh(entityId, m, t.GetCombined());
         }
     });
 }
@@ -83,24 +83,67 @@ void BatchedRenderer::Update(float dt)
     projection = glm::perspective(glm::radians(90.0f), (float)engine->width/engine->height, 0.1f, 100.0f);
     engine->camera.CalculateFrustum((float)engine->width / (float)engine->height, 90.0f, 0.1f, 100.0f);
 
+    //New chunk testing
+    TransformComponent CameraTransform;
+    CameraTransform.position = engine->camera.m_cameraPosition;
+
+    chunkMgr.Load(CameraTransform); //Load the chunk where the camera is located
+
+    /*
+    Move to better verision later where if loading new chunk, 
+    generate GPuMemoryHandles for entities in that chunk
+
+    When unloading chunk, remove GPUMemoryHandles for entities in that chunk
+
+    then use regular render loop
+
+    */
+
+    //Regular frustum culling and batching
     std::unordered_map<int, std::vector<int>> meshGroups;
     std::vector<int> lookupTable;
     std::vector<GPUMemoryHandle> finalCommands;
 
 
-    //TODO: fetch from chunkmanager
-    ecs->view<BufferedMesh, GPUMemoryHandle, TransformComponent>().each([&](int entityId, BufferedMesh& m, GPUMemoryHandle& h, TransformComponent& t) {
-        
+    std::vector<Entity> visibleEntities = chunkMgr.GetLoadedEntities();
+    //std::cout << "Visible Entities in Loaded Chunks: " << visibleEntities.size() << std::endl;
+    for(Entity e : visibleEntities){
+    
+        if(!ecs->HasComponent<GPUMemoryHandle>(e)) continue;
+        GPUMemoryHandle& h = ecs->GetComponent<GPUMemoryHandle>(e);;
+        TransformComponent& t = ecs->GetComponent<TransformComponent>(e);
+
         if (t.isDirty) {
             batch->UpdateTransform(h.ssboIndex, t.GetCombined());
             t.isDirty = false;
         }
-        
-        //Frustum Culling
-        if (!engine->camera.isVisible(t)) return;
-        
+
+        if (!engine->camera.isVisible(t)) continue;
+
+        if(!ecs->HasComponent<BufferedMesh>(e)) continue;
+        BufferedMesh& m = ecs->GetComponent<BufferedMesh>(e);
+            
         meshGroups[m.meshID].push_back(h.ssboIndex);
-    });
+
+        //std::cout << "Entity " << e << " with MeshID " << m.meshID << " is visible." << std::endl;
+    }
+
+    /*
+
+    //TODO: fetch from chunkmanager
+     ecs->view<BufferedMesh, GPUMemoryHandle, TransformComponent>().each([&](int entityId, BufferedMesh& m, GPUMemoryHandle& h, TransformComponent& t) {
+        
+         if (t.isDirty) {
+             batch->UpdateTransform(h.ssboIndex, t.GetCombined());
+             t.isDirty = false;
+         }
+        
+         //Frustum Culling
+         if (!engine->camera.isVisible(t)) return;
+        
+         meshGroups[m.meshID].push_back(h.ssboIndex);
+     });
+     */
 
     int count = 0;
     for (auto& [meshID, instances] : meshGroups) {
